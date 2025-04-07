cc96aec4313108174505410d4bcd3a52d1d71e9f
Olaiya Olayimika <64112251+yinks002@users.noreply.github.com>
2025-04-07 21:32:04 +0100

    Add files via upload

--- components/Statistics.jsx
+++ components/Statistics.jsx
@@ -58,6 +58,75 @@ const Statistics = () => {
           </div>
         </div>
 
+  <div className="col-12">
+    <div className="content">
+      <div className="bg-pic">
+        <img src="./assets/img/stasictic-bg.png" alt="" />
+      </div>
+      <div className="section-head text-center wow fadeInUp"
+       data-wow-duration = "0.5s"
+        data-wow-delay= "0.5s"
+      >
+        <h4 className="lasthead">Out stats say more than our words</h4>
+        <h2 className="title">Today's statistics</h2>
+        <p className="text">Lorem ipsum dolor sit, 
+          amet consec  
+          est hic natus magnam?</p>
+
+      </div>
+
+  <div className="row justify-content-center">
+    <div className="col-xl-4 col-lg-6 wow fadeInUp"
+     data-wow-duration = "0.5s"
+     data-wow-delay= "0.5s"
+     >
+      <div className="statis-boxx">
+        <div className="icon">
+          <img src="assets/img/statistic-icon-1.png" alt="" />
+        </div>
+        <div className="statis-content">
+          <h3 className="suntitle">984</h3>
+          <p className="text">Transactions made</p>
+        </div>
+      </div>
+     </div>
+
+     <div className="col-xl-4 col-lg-6 wow fadeInUp"
+     data-wow-duration = "0.5s"
+     data-wow-delay= "0.5s"
+     >
+      <div className="statis-boxx">
+        <div className="icon">
+          <img src="assets/img/statistic-icon-1.png" alt="" />
+        </div>
+        <div className="statis-content">
+          <h3 className="suntitle">7 Minutes</h3>
+          <p className="text">Average processing time </p>
+        </div>
+      </div>
+     </div>
+
+
+     <div className="col-xl-4 col-lg-6 wow fadeInUp"
+     data-wow-duration = "0.5s"
+     data-wow-delay= "0.5s"
+     >
+      <div className="statis-boxx">
+        <div className="icon">
+          <img src="assets/img/statistic-icon-1.png" alt="" />
+        </div>
+        <div className="statis-content">
+          <h3 className="suntitle">Eth/YKT</h3>
+          <p className="text">Today's favorite pair</p>
+        </div>
+      </div>
+     </div>
+  </div>
+
+
+    </div>
+  </div>
+
 
       </div>
     </div>
