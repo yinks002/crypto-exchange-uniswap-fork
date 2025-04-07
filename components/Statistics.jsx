54de768c9319c28b90c308bedd74ab753ca0530f
Olaiya Olayimika <64112251+yinks002@users.noreply.github.com>
2025-04-07 21:06:28 +0100

    Add files via upload

--- components/Statistics.jsx
+++ components/Statistics.jsx
@@ -17,9 +17,33 @@ const Statistics = () => {
           </div>
         </div>
 
+
         <div className="col-lg-3 col-sm-6 wow fadeInUp"
-        data-wow-duration = "0.4"
-        data-wow-delay= "0.4s"></div>
+        data-wow-duration = "0.4s"
+        data-wow-delay= "0.4"
+        >
+
+          <div className="platform-box text-center">
+            <div className="icon">
+              <img src="assets/img/offer-icon-2.png" alt="" />
+            </div>
+            <p className="text">secure  <br /> transaction </p>
+          </div>
+        </div>
+
+
+        <div className="col-lg-3 col-sm-6 wow fadeInUp"
+        data-wow-duration = "0.5s"
+        data-wow-delay= "0.5s"
+        >
+
+          <div className="platform-box text-center">
+            <div className="icon">
+              <img src="assets/img/offer-icon-3.png" alt="" />
+            </div>
+            <p className="text">No limits on  <br /> exchange</p>
+          </div>
+        </div>
 
       </div>
     </div>
