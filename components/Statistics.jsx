dbe97187323e53bdd88204803b6d1b9e2eac24f3
Olaiya Olayimika <64112251+yinks002@users.noreply.github.com>
2025-04-07 21:01:38 +0100

    Add files via upload

--- components/Statistics.jsx
+++ components/Statistics.jsx
@@ -3,7 +3,20 @@ import React from "react";
 const Statistics = () => {
   return <div className="statistic">
     <div className="container">
-      <div className="row"></div>
+      <div className="row">
+        <div className="col-lg-3 col-sm-6 wow fadeInUp"
+        data-wow-duration = "0.3s"
+        data-wow-delay= "0.5s"
+        >
+
+          <div className="platform-box text-center">
+            <div className="icon">
+              <img src="assets/img/offer-icon-1.png" alt="" />
+            </div>
+            <p className="text">Extra fast <br /> </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>;
 };
